ab8fece3669b999f0cb9f5dddbf7bd184be4baa0
Anjan Diyora <anjanjee11@gmail.com>
2022-05-06 10:15:53 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x2777eF1C7a506df8ee51766777E0246e0Dc3F527";
+export const contractABI = abi.abi;
---
+++ src/utils/dummyData.js
@@ -0,0 +1,12 @@
+
+export default [
+  {
+    id: 1,
+    url: "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284",
+    message: "Dummy Transaction",
+    timestamp: "05/05/2022, 14:26:19 PM",
+    addressFrom: "0x524948A4334193fc453640172cd70224d5A81622",
+    amount: "0.01",
+    addressTo: "0x0445F39d9A83bEe69B159a42AFde2e3566fe90D5",
+  }
+];
